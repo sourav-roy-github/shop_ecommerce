b94047801be3a0315b679825730c1e260a5d6a39
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-18 19:04:21 -0400

    Update order - admin

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
-const { newOrder } = require('../controllers/orderController')
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  allOrders,
+  updateOrder,
+} = require('../controllers/orderController')
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder)
+
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder)
+router.route('/orders/me').get(isAuthenticatedUser, myOrders)
+
+router
+  .route('/admin/orders/')
+  .get(isAuthenticatedUser, authorizeRoles('admin'), allOrders)
+
+router
+  .route('/admin/order/:id')
+  .put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder)
 
 module.exports = router
