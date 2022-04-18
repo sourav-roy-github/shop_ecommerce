9922fdb75d7a1fa08c33e887f5d208cf09d588ba
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-17 20:46:25 -0400

    Creating order controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,5 @@
+const Order = require('../models/order')
+const Product = require('../models/product')
+
+const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
