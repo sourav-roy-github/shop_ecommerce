ae17fbea18f568025bcde1f8ce32593c26527c3a
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-20 14:56:55 -0400

    Delete review route added

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -9,6 +9,7 @@ const {
   deleteProduct,
   createProductReview,
   getProductReviews,
+  deleteReview,
 } = require('../controllers/productController')
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
@@ -31,5 +32,6 @@ router
 
 router.route('/review').put(isAuthenticatedUser, createProductReview)
 router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
+router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
 
 module.exports = router
