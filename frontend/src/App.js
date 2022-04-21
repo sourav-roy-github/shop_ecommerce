32a395c40e35c69e1c2e94cdd6bcb75252554ed8
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-21 14:38:22 -0400

    Navbar and footer section added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   )
 }
---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+import '../../App.css'
+
+const Footer = () => {
+  return (
+    <Fragment>
+      <footer className="py-1">
+        <p className="text-center mt-1">
+          Artilugio - 2022, All Rights Reserved
+        </p>
+      </footer>
+    </Fragment>
+  )
+}
+
+export default Footer
