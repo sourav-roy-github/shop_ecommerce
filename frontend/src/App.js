885218bbeec67cbb62397fe554f5eb35646f54bb
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-21 21:06:11 -0400

    Added product details component and mapped the title, id, description, image with the backend data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/Home'
+import ProductDetails from './components/product/ProductDetails'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <div className="container container-fluid">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
           </Routes>
         </div>
         <Footer />
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -31,13 +31,14 @@ export const getProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST })
 
-    const { data } = await axios.get(`/api/v1/product/${id}`)
+    const { data } = await axios.get(`/api/v1/products/${id}`)
 
     dispatch({
       type: PRODUCT_DETAILS_SUCCESS,
       payload: data.product,
     })
   } catch (error) {
+    //console.log(error.response)
     dispatch({
       type: PRODUCT_DETAILS_FAIL,
       payload: error.response.data.message,
