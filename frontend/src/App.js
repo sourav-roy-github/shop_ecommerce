1dda6c38664c04a1e555f0d75613963c0d4238b4
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-22 18:05:29 -0400

    Login credentials verified with the database details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/Home'
 import ProductDetails from './components/product/ProductDetails'
+import Login from './components/user/Login'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/search/:keyword" element={<Home />} />
             <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
         <Footer />
