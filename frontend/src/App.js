0174d9aec09d03826a37dbfdf7915ca86f847bfb
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-22 21:19:07 -0400

    User profile updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,11 @@ import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/Home'
 import ProductDetails from './components/product/ProductDetails'
+
 import Login from './components/user/Login'
 import Register from './components/user/Register'
+import Profile from './components/user/Profile'
+
 import { loadUser } from './actions/userActions'
 import store from './store'
 
@@ -25,6 +28,7 @@ function App() {
             <Route path="/product/:id" element={<ProductDetails />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/me" element={<Profile />} />
           </Routes>
         </div>
         <Footer />
