5b48f120794872372ff4706725b61e980755666e
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-25 19:59:16 -0400

    New product component added

---
+++ frontend/src/components/admin/NewProduct.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useState, useEffect } from 'react'
+
+import MetaData from '../layout/MetaData'
+import Sidebar from './Sidebar'
+
+import { useAlert } from 'react-alert'
+import { useDispatch, useSelector } from 'react-redux'
+
+const NewProduct = () => {
+  return <div></div>
+}
+
+export default NewProduct
