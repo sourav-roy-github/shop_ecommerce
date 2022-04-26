1e3ff12feb2dda21e348cc881828e304ac41c454
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-25 20:01:24 -0400

    Product attributes added for newProduct component

--- frontend/src/components/admin/NewProduct.js
+++ frontend/src/components/admin/NewProduct.js
@@ -2,11 +2,21 @@ import React, { Fragment, useState, useEffect } from 'react'
 
 import MetaData from '../layout/MetaData'
 import Sidebar from './Sidebar'
-
+import { useNavigate } from 'react-router-dom'
 import { useAlert } from 'react-alert'
 import { useDispatch, useSelector } from 'react-redux'
+import { newProduct, clearErrors } from '../../actions/productActions'
 
-const NewProduct = () => {
+const NewProduct = ({ history }) => {
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState(0)
+  const [description, setDescription] = useState('')
+  const [category, setCategory] = useState('')
+  const [stock, setStock] = useState(0)
+  const [seller, setSeller] = useState('')
+  const [images, setImages] = useState([])
+  const [imagesPreview, setImagesPreview] = useState([])
+
   return <div></div>
 }
 
