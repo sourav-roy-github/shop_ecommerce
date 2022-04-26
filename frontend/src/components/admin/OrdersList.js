78313ceb81b87afea124527b31690ff074900d2e
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-25 22:36:39 -0400

    Orders list component updated

--- frontend/src/components/admin/OrdersList.js
+++ frontend/src/components/admin/OrdersList.js
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { MDBDataTable } from 'mdbreact'
 
-const OrdersList = () => {
+import MetaData from '../layout/MetaData'
+import Loader from '../layout/Loader'
+import Sidebar from './Sidebar'
+
+import { useAlert } from 'react-alert'
+import { useDispatch, useSelector } from 'react-redux'
+import { allOrders, clearErrors } from '../../actions/orderActions'
+//import { DELETE_ORDER_RESET } from '../../constants/orderConstants'
+
+const OrdersList = ({ history }) => {
+  const alert = useAlert()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { loading, error, orders } = useSelector((state) => state.allOrders)
+  //const { isDeleted } = useSelector((state) => state.order)
+
+  useEffect(() => {
+    dispatch(allOrders())
+
+    if (error) {
+      alert.error(error)
+      dispatch(clearErrors())
+    }
+
+    if (isDeleted) {
+      alert.success('Order deleted successfully')
+      navigate('/admin/orders')
+      dispatch({ type: DELETE_ORDER_RESET })
+    }
+  }, [dispatch, alert, error, isDeleted, history])
+
   return <div></div>
 }
 
