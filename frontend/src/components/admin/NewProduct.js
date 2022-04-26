9e1ee36b3ba05bc6bd354335f565c0126f9c7726
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-25 20:05:10 -0400

    useEffect function added for newProduct component

--- frontend/src/components/admin/NewProduct.js
+++ frontend/src/components/admin/NewProduct.js
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom'
 import { useAlert } from 'react-alert'
 import { useDispatch, useSelector } from 'react-redux'
 import { newProduct, clearErrors } from '../../actions/productActions'
+import { NEW_PRODUCT_RESET } from '../../constants/productConstants'
 
 const NewProduct = ({ history }) => {
   const [name, setName] = useState('')
@@ -16,6 +17,38 @@ const NewProduct = ({ history }) => {
   const [seller, setSeller] = useState('')
   const [images, setImages] = useState([])
   const [imagesPreview, setImagesPreview] = useState([])
+
+  const categories = [
+    'Electronics',
+    'Cameras',
+    'Laptops',
+    'Accessories',
+    'Headphones',
+    'Food',
+    'Books',
+    'Clothes/Shoes',
+    'Beauty/Health',
+    'Sports',
+    'Outdoor',
+    'Home',
+  ]
+
+  const alert = useAlert()
+  const dispatch = useDispatch()
+
+  const { loading, error, success } = useSelector((state) => state.newProduct)
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error)
+      dispatch(clearErrors())
+    }
+    if (success) {
+      navigate('/admin/products')
+      alert.success('Product created successfully')
+      dispatch({ type: NEW_PRODUCT_RESET })
+    }
+  }, [dispatch, alert, error, success, history])
 
   return <div></div>
 }
