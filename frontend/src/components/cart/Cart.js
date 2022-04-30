fdbfa72cb615a8ef1d639a84de801d3a63bb79e3
sourav-roy-github <roy.souravv25@gmail.com>
2022-04-30 11:36:02 -0400

    Decimal value restricted to 2 places - cart items

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -76,7 +76,7 @@ const Cart = ({ history }) => {
                       </div>
 
                       <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                        <p id="card_item_price">${item.price}</p>
+                        <p id="card_item_price">${item.price}.toFixed(2)</p>
                       </div>
 
                       <div className="col-4 col-lg-3 mt-4 mt-lg-0">
@@ -139,7 +139,9 @@ const Cart = ({ history }) => {
                 </p>
                 <p>
                   Est. total:{' '}
-                  <span className="order-summary-values">${totalPrice}</span>
+                  <span className="order-summary-values">
+                    ${totalPrice}.toFixed(2)
+                  </span>
                 </p>
 
                 <hr />
